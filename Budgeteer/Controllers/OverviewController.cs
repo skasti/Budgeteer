816910967ff3040ebfe4f8255bc9f08490a9ec97
skasti <stig-rune@derailgames.no>
2015-10-27 22:11:36 +0100

    Viser oversikt over kontoer + balanse

---
+++ Budgeteer/Controllers/OverviewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.Extensions;
+
+namespace Budgeteer.Controllers
+{
+    public class OverviewController: IController
+    {
+
+        public string Name
+        {
+            get { return "Overview"; }
+        }
+
+        public bool Execute()
+        {
+            Console.Clear();
+            Console.WriteLine("Accounts:");
+            
+            foreach (var account in BudgeteerData.Accounts)
+            {
+                Console.WriteLine(String.Format("{0} ({1}): {2}", account.Name, account.AccountNumber, account.GetBalance(BudgeteerData.Transactions, DateTime.Now)));
+            }
+
+            Console.WriteLine("\n(press enter to return to menu)");
+            Console.ReadLine();
+            Program.ChangeController("Menu", true);
+            return true;
+        }
+
+        public void Reset()
+        {
+            
+        }
+    }
+}
