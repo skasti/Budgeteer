d9c28d2cbe844bd2a0ecafb3840b54b5a704530e
skasti <stig-rune@derailgames.no>
2015-10-27 22:09:31 +0100

    Laget interface for controllere

---
+++ Budgeteer/Controllers/IController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budgeteer.Controllers
+{
+    public interface IController
+    {
+        string Name { get; }
+        bool Execute();
+        void Reset();
+    }
+}
