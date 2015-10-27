9d9be8a5b9f8889e59e3feaeb2c1373966ffd3a4
skasti <stig-rune@derailgames.no>
2015-10-27 22:12:26 +0100

    Begynt på lasting

---
+++ Budgeteer/Controllers/LoadTransactionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budgeteer.Controllers
+{
+    public class LoadTransactionsController: IController
+    {
+        enum LoadTransactionsState
+        {
+            Account, File, Progress, Summary
+        }
+
+        public string Name
+        {
+            get { return "LoadTransactions"; }
+        }
+
+        public bool Execute()
+        {
+            
+        }
+
+        public void Reset()
+        {
+            
+        }
+    }
+}
---
+++ Budgeteer/Import/Data/CsvEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budgeteer.Import.Data
+{
+    public class CsvEntry
+    {
+        //Dato;Beskrivelse;Rentedato;Ut/Inn;
+        public DateTime Date { get; private set; }
+        public string Description { get; private set; }
+        public DateTime RateDate { get; private set; }
+        public double Amount { get; private set; }
+
+        public CsvEntry(DateTime date, string description, DateTime rateDate, double amount)
+        {
+            Date = date;
+            Description = description;
+            RateDate = rateDate;
+            Amount = amount;
+        }
+    }
+}
