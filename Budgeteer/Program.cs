d93d2d663a73a2a7f626ad73bab539121181b761
skasti <stig-rune@derailgames.no>
2015-10-27 22:10:39 +0100

    Satt opp rammeverk for å kunne bytte controllere osv

--- Budgeteer/Program.cs
+++ Budgeteer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Budgeteer.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,57 @@ using System.Threading.Tasks;
 
 namespace Budgeteer
 {
-    class Program
+    public class Program
     {
+        static IEnumerable<IController> Controllers = new List<IController>
+        {
+        };
+
+        static IController currentController;
+        static IController nextController;
+        static bool resetNext = false;
+
+        public static void ChangeController(string controllerName, bool reset)
+        {
+            var controller = Controllers.FirstOrDefault(c => c.Name == controllerName);
+
+            if (controller == null)
+                throw new ArgumentException("Could not find controller '" + controllerName + "'");
+
+            nextController = controller;
+            resetNext = reset;
+        }
+
         static void Main(string[] args)
         {
+
+            var running = true;
+
+            while (running)
+            {
+                try
+                {
+                    running = currentController.Execute();
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+
+                SwapControllers();
+            }
+        }
+
+        private static void SwapControllers()
+        {
+            if (nextController != null)
+            {
+                currentController = nextController;
+                if (resetNext) currentController.Reset();
+
+                nextController = null;
+            }
         }
     }
 }
