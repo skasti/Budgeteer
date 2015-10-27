8850d8a10793606b0980e4d5745e81467c53a198
skasti <stig-rune@derailgames.no>
2015-10-27 22:08:38 +0100

    Lagt til en klasse for å holde statiske data

---
+++ Budgeteer/BudgeteerData.cs
@@ -0,0 +1,15 @@
+﻿using Accounting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budgeteer
+{
+    public class BudgeteerData
+    {
+        public static IList<Transaction> Transactions = new List<Transaction>();
+        public static IList<Account> Accounts = new List<Account>();
+    }
+}
